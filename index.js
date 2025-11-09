ec00f6080c17db3f5629a43e0572a7d9c6cdc7f4
moinulhasan45777 <moinul.hasan45777@gmail.com>
2025-11-10 02:14:41 +0600

    Connected MongoDB with Server

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
-const cors = require("cores");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://moinul45777:qxl22HBK0kFl0RUp@moinul.ciuiphx.mongodb.net/?appName=Moinul";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
