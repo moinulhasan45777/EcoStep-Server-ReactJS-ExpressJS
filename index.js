4538fb469f43c70867eb85dec6a1fbfce2292d55
moinulhasan45777 <moinul.hasan45777@gmail.com>
2025-11-12 04:36:33 +0600

    Added Get API for Tips

--- index.js
+++ index.js
@@ -21,23 +21,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+
+    const db = client.db("eco-step_db");
+    const tipsCollection = db.collection("tips");
+
+    app.get("/tips", async (req, res) => {
+      const cursor = tipsCollection.find().limit(3);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
