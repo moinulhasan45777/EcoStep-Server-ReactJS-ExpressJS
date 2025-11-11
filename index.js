88a7801a931bbac262c8a4395ddb00c3cb6da0b5
moinulhasan45777 <moinul.hasan45777@gmail.com>
2025-11-12 05:57:03 +0600

    Added Challenge Filtration

--- index.js
+++ index.js
@@ -24,9 +24,10 @@ async function run() {
 
     const db = client.db("eco-step_db");
 
-    // Collections
+    // Collections -------------------
     const tipsCollection = db.collection("tips");
     const eventsCollection = db.collection("events");
+    const challengesCollection = db.collection("challenges");
 
     app.get("/tips", async (req, res) => {
       const cursor = tipsCollection.find().limit(3);
@@ -34,7 +35,28 @@ async function run() {
       res.send(result);
     });
     app.get("/events", async (req, res) => {
-      const cursor = tipsCollection.find().limit(3);
+      const cursor = eventsCollection.find().limit(3);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/challenges", async (req, res) => {
+      const query = {};
+
+      if (req.query.category) {
+        query.category = { $in: [req.query.category] };
+      }
+      if (req.query.startDate) {
+        query.startDate = { $gte: req.query.startDate };
+      }
+      if (req.query.endDate) {
+        query.endDate = { $lte: req.query.endDate };
+      }
+      if (req.query.participants) {
+        query.participants.$gte = { $gte: 0 };
+        query.participants.$lte = { $lte: parseInt(req.query.participants) };
+      }
+
+      const cursor = challengesCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
